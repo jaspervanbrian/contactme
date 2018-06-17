0fd57c3e2c3052c8163dfe34aa9f11441674d824
Jasper Martin <jaspervanbrianmartin@gmail.com>
2018-06-17 13:09:21 +0800

    Added simple 404 page.

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,8 +3,28 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Oops! 404.</title>
+	<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+	<style>
+		.margin-50 {
+			margin: 50px;
+		}
+		html, body {
+			background-color: #353334;
+		}
+	</style>
 </head>
 <body>
-	
+	<main>
+		<div class="container">
+			<div class="row">
+				<div class="col-12 margin-50 d-flex flex-wrap align-content-center justify-content-center">
+					<div class="jumbotron">
+						<h1>Oops. 404!</h1>
+						<p>Sorry, requested page doesn't exist!</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
 </body>
 </html>
